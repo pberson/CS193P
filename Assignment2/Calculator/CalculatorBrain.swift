8bcbf1ef9e760a7bbae36ec65b3865e165291581
Peter Berson <pberson@gmail.com>
2017-02-23 13:22:11 -0500

    Add Validation for operations

--- Assignment2/Calculator/CalculatorBrain.swift
+++ Assignment2/Calculator/CalculatorBrain.swift
@@ -38,6 +38,8 @@ class CalculatorBrain {
 
     var isPartialResult = false
     
+    var operationError: String?
+    
     func setOperand(operand: Double) {
         // if we press an operand with no pending operator reset
         if pending == nil { clear () }
@@ -89,30 +91,42 @@ class CalculatorBrain {
             clear()
         }
     }
+ 
+    // TODO testing out new error checking.
+    private func divide (x: Double, y: Double) -> (result: Double, error: String?) {
+        return ( x != 0) ? (x/y,nil) : (x/y, "Error")
+    }
+    
+    private func my_sqrt (x: Double) -> (result: Double, error: String?) {
+        return  x >= 0 ? (sqrt(x),nil) : (sqrt(x), "Error")
+    }
     
     // first closure is the operation second is the pretty string for description
     private var operations: Dictionary<String,Operation> = [
         "π" : Operation.Constant(M_PI),
         "e" : Operation.Constant(M_E),
         "rand" : Operation.NonOperandOperation(drand48, {"rand (\($0))"}),
-        "±" : Operation.UnaryOperation({ -$0 }, {"-(\($0))"}),
-        "√" : Operation.UnaryOperation(sqrt, {"√(\($0))"}),
-        "cos" : Operation.UnaryOperation(cos, {"cos(\($0))"}),
-        "sin" : Operation.UnaryOperation(sin, {"sin(\($0))"}),
-        "tan" : Operation.UnaryOperation(tan, {"tan(\($0))"}),
-        "x²" : Operation.UnaryOperation({pow($0,2)}, {"(\($0))²"}),
-        "×" : Operation.BinaryOperation({ $0 * $1 }, { "\($0) × \($1)" }, Precedence.Max),
-        "÷" : Operation.BinaryOperation({ $0 / $1 }, { "\($0) ÷ \($1)" }, Precedence.Max),
-        "-" : Operation.BinaryOperation({ $0 - $1 }, { "\($0) - \($1)" }, Precedence.Min),
-        "+" : Operation.BinaryOperation({ $0 + $1 }, { "\($0) + \($1)" }, Precedence.Min),
+        "±" : Operation.UnaryOperation({ (-$0,nil) }, {"-(\($0))"}),
+        "√" : Operation.UnaryOperation( {$0 >= 0 ? (sqrt($0), nil) : (sqrt($0),"Math Error operand \($0) not valid!")} , {"√(\($0))"}),
+        "cos" : Operation.UnaryOperation({(cos($0),nil)}, {"cos(\($0))"}),
+        "sin" : Operation.UnaryOperation({(sin($0),nil)}, {"sin(\($0))"}),
+        "tan" : Operation.UnaryOperation({(tan($0),nil)}, {"tan(\($0))"}),
+        "x²" : Operation.UnaryOperation({(pow($0,2), nil)}, {"(\($0))²"}),
+        "×" : Operation.BinaryOperation({ ($0 * $1, nil) }, { "\($0) × \($1)" }, Precedence.Max),
+        "÷" : Operation.BinaryOperation({ $1 != 0 ? ($0 / $1, nil) : ($0 / $1, "Math Error Div Zero") }, { "\($0) ÷ \($1)" }, Precedence.Max),
+        "-" : Operation.BinaryOperation({ ($0 - $1, nil) }, { "\($0) - \($1)" }, Precedence.Min),
+        "+" : Operation.BinaryOperation({ ($0 + $1, nil) }, { "\($0) + \($1)" }, Precedence.Min),
         "=" : Operation.Equals,
         ]
     
+    // Unary and Binary operations accept Doubles and (tuple) return Double and Error String 
+    // The other function (except for Constant and Equals) have a secondary function that builds the descprtion string
+    // These is taking advantage of Swift Function Closure capablities
     private enum Operation {
         case Constant(Double)
         case NonOperandOperation(() -> Double, (String) ->String)
-        case UnaryOperation((Double) -> Double, (String) -> String)
-        case BinaryOperation((Double, Double) -> Double, (String,String) -> String, Precedence)
+        case UnaryOperation((Double) -> (result: Double, errorString: String?), (String) -> String)
+        case BinaryOperation((Double, Double) -> (result: Double, errorString: String?), (String,String) -> String, Precedence)
         case Equals
     }
     
@@ -128,7 +142,7 @@ class CalculatorBrain {
                 accumulator = function()
                 descriptionAccumulator = descriptionFunction(String(formatNumber(numAsDouble: accumulator)))
             case .UnaryOperation(let function, let descriptionFunction):
-                accumulator = function(accumulator)
+                (accumulator,operationError) = function(accumulator)
                 descriptionAccumulator = descriptionFunction(descriptionAccumulator)
             case .BinaryOperation(let function, let descriptionFunction, let precedence):
                 executePendingBinaryOperation()
@@ -138,7 +152,7 @@ class CalculatorBrain {
                 }
                 currrentPrecedence = precedence
                 pending = PendingBinaryOperationInfo(binaryFunction: function, firstOperand: accumulator,
-                                                     descriptionFunction: descriptionFunction, firstDescriptionOperand: descriptionAccumulator )
+                                                     descriptionFunction: descriptionFunction, firstDescriptionOperand: descriptionAccumulator)
             case .Equals:
                 executePendingBinaryOperation()
             }
@@ -147,7 +161,7 @@ class CalculatorBrain {
     
     private func executePendingBinaryOperation() {
         if pending != nil {
-            accumulator = pending!.binaryFunction(pending!.firstOperand, accumulator)
+            (accumulator,operationError) = pending!.binaryFunction(pending!.firstOperand, accumulator)
             descriptionAccumulator = pending!.descriptionFunction(pending!.firstDescriptionOperand, descriptionAccumulator)
             pending = nil
             isPartialResult = false
@@ -159,7 +173,7 @@ class CalculatorBrain {
     private var pending: PendingBinaryOperationInfo?
     
     private struct PendingBinaryOperationInfo {
-        var binaryFunction: (Double, Double) -> Double
+        var binaryFunction: (Double, Double) -> (Double, String?)
         var firstOperand: Double
         var descriptionFunction: (String, String) -> String
         var firstDescriptionOperand: String
